af041aec0834a58f481f6649e42ff9869520d247
Antti <anttis.pitkanen@gmail.com>
2016-03-30 23:21:01 +0300

    I4

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,14 +1,29 @@
 
 require "csv"
+require "sunlight/congress"
+require "erb"
+
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
 end
 
+def legislators_by_zipcode(zipcode)
+  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+end
+
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+
+template_letter = File.read("form_letter.erb")
+erb_template = ERB.new(template_letter)
 
 contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
-  puts "#{name} #{zipcode}"
+  legislators = legislators_by_zipcode(zipcode)
+
+  form_letter = erb_template.result(binding)
+
+  puts form_letter
 end
